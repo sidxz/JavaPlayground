0119791a390fb521cde62d22465b66e4cc01257d
sidxz <sid@tamu.edu>
2017-03-21 10:36:57 -0500

    primitives:CountingBits First commit

---
+++ primitives/CountingBits.java
@@ -0,0 +1,30 @@
+package me.sidx.primitives;
+
+public class CountingBits {
+	
+	/* Program to count number of bits that are set to 1
+	 * 
+	 */
+	
+	public static short countBits(int x) {
+		short numBits = 0;
+		
+		while (0 != x) {
+			numBits += (x & 1);
+			x >>>= 1;
+		}
+		
+		
+		return numBits;
+	}
+	
+	
+	public static void main(String[] args) {
+	
+		int x = 5;
+		
+		System.out.println("No of Bits in " + x + " is " + countBits(5));
+		
+	}
+
+}
